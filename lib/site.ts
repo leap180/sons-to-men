7babe25b06d4cefca8634cafecb279d8bd22e28c
leap180 <clubritual13@gmail.com>
2026-08-23 19:20:16 -0400

    Rename STRIPE URLs to STRIPE URLs

--- lib/site.ts
+++ lib/site.ts
@@ -1,7 +1,7 @@
 // Update these to your live Selar product links.
-export const SELAR_URL = "https://buy.stripe.com/dRm14o6BU2xk1VK8aP18c01"
-export const SELAR_MONTHLY_URL = "https://buy.stripe.com/dRm14o6BU2xk1VK8aP18c01"
-export const SELAR_YEARLY_URL = "https://buy.stripe.com/dRm14o6BU2xk1VK8aP18c01"
-export const SELAR_URL_MONTHLY = "https://buy.stripe.com/dRm14o6BU2xk1VK8aP18c01"
+export const STRIPE_MONTHLY_URL = "https://buy.stripe.com/dRm14o6BU2xk1VK8aP18c01"
+export const STRIPE_YEARLY_URL =  "https://buy.stripe.com/dRm14o6BU2xk1VK8aP18c01"
+export const STRIPE_URL_MONTHLY =  "https://buy.stripe.com/dRm14o6BU2xk1VK8aP18c01"
+export const STRIPE_URL_MONTHLY = "https://buy.stripe.com/dRm14o6BU2xk1VK8aP18c01"
 export const FACEBOOK_URL = "https://www.facebook.com/share/g/18HAXUbZEr/"
 
