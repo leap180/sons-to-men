11a6b190f4e13d8aa881db73bdda04f51b838473
leap180 <clubritual13@gmail.com>
2026-08-23 19:05:33 -0400

    Add SELAR_URL_MONTHLY constant

--- lib/site.ts
+++ lib/site.ts
@@ -4,3 +4,4 @@ export const SELAR_MONTHLY_URL = "https://buy.stripe.com/dRm14o6BU2xk1VK8aP18c01
 export const SELAR_YEARLY_URL = "https://buy.stripe.com/dRm14o6BU2xk1VK8aP18c01"
 export const SELAR_URL_MONTHLY = "https://buy.stripe.com/dRm14o6BU2xk1VK8aP18c01"
 export const FACEBOOK_URL = "https://www.facebook.com/share/g/18HAXUbZEr/"
+
