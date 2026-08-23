49f15e5c50fae013e8fa3d2a4abb5121d76ac376
leap180 <clubritual13@gmail.com>
2026-08-23 19:55:45 -0400

    Add STRIPE_URL constant for Stripe payment link

--- lib/site.ts
+++ lib/site.ts
@@ -4,4 +4,4 @@ export const STRIPE_YEARLY_URL =  "https://buy.stripe.com/dRm14o6BU2xk1VK8aP18c0
 export const STRIPE_URL_MONTHLY =  "https://buy.stripe.com/dRm14o6BU2xk1VK8aP18c01"
 export const STRIPE_URL_YEARLY= "https://buy.stripe.com/dRm14o6BU2xk1VK8aP18c01"
 export const FACEBOOK_URL = "https://www.facebook.com/share/g/18HAXUbZEr/"
-
+export const STRIPE_URL = "https://buy.stripe.com/dRm14o6BU2xk1VK8aP18c01"
