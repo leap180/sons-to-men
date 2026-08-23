50f2ff07f53d3a8d61f627fa32239e47e456b89c
leap180 <clubritual13@gmail.com>
2026-08-23 18:42:47 -0400

    Update Selar product links to Stripe URLs

--- lib/site.ts
+++ lib/site.ts
@@ -1,6 +1,6 @@
 // Update these to your live Selar product links.
-export const SELAR_URL = "https://selar.com/p1j8a1u1e8"
-export const SELAR_MONTHLY_URL = "https://selar.com/p1j8a1u1e8"
-export const SELAR_YEARLY_URL = "https://selar.com/p1j8a1u1e8"
-export const SELAR_URL_MONTHLY = "https://selar.com/p1j8a1u1e8"
+export const SELAR_URL = "https://buy.stripe.com/dRm14o6BU2xk1VK8aP18c01"
+export const SELAR_MONTHLY_URL = "https://buy.stripe.com/dRm14o6BU2xk1VK8aP18c01"
+export const SELAR_YEARLY_URL = "https://buy.stripe.com/dRm14o6BU2xk1VK8aP18c01"
+export const SELAR_URL_MONTHLY = "https://buy.stripe.com/dRm14o6BU2xk1VK8aP18c01"
 export const FACEBOOK_URL = "https://www.facebook.com/share/g/18HAXUbZEr/"
