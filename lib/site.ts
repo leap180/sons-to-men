749c2025ea39b3ca9b1729b20edb9b220fa78438
leap180 <clubritual13@gmail.com>
2026-08-23 19:45:00 -0400

    Fix Stripe URL constant for yearly subscription

--- lib/site.ts
+++ lib/site.ts
@@ -2,6 +2,6 @@
 export const STRIPE_MONTHLY_URL = "https://buy.stripe.com/dRm14o6BU2xk1VK8aP18c01"
 export const STRIPE_YEARLY_URL =  "https://buy.stripe.com/dRm14o6BU2xk1VK8aP18c01"
 export const STRIPE_URL_MONTHLY =  "https://buy.stripe.com/dRm14o6BU2xk1VK8aP18c01"
-export const STRIPE_URL_MONTHLY = "https://buy.stripe.com/dRm14o6BU2xk1VK8aP18c01"
+export const STRIPE_URL_YEARLY= "https://buy.stripe.com/dRm14o6BU2xk1VK8aP18c01"
 export const FACEBOOK_URL = "https://www.facebook.com/share/g/18HAXUbZEr/"
 
