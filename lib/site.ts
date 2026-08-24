eb18ee354a94ce121162809a858cffa7e7c2caa9
leap180 <clubritual13@gmail.com>
2026-08-23 20:08:01 -0400

    Add SELAR URL aliases for backward compatibility
    Added temporary aliases for SELAR URLs to maintain compatibility with old code.

--- lib/site.ts
+++ lib/site.ts
@@ -5,3 +5,7 @@ export const STRIPE_URL_MONTHLY =  "https://buy.stripe.com/dRm14o6BU2xk1VK8aP18c
 export const STRIPE_URL_YEARLY= "https://buy.stripe.com/dRm14o6BU2xk1VK8aP18c01"
 export const FACEBOOK_URL = "https://www.facebook.com/share/g/18HAXUbZEr/"
 export const STRIPE_URL = "https://buy.stripe.com/dRm14o6BU2xk1VK8aP18c01"
+// Temporary aliases so old code still works
+export const SELAR_URL = STRIPE_URL
+export const SELAR_URL_MONTHLY = STRIPE_URL
+export const SELAR_URL_YEARLY = STRIPE_URL
